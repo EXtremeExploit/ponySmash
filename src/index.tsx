ce8456d102e45559cc62e13505120c8a6efa2cda
EXtremeExploit <pedro.montes.alcalde@gmail.com>
2023-12-18 02:04:17 -0300

    Convert main index into tsx

---
+++ src/index.tsx
@@ -0,0 +1,17 @@
+import initReactFastclick from 'react-fastclick';
+
+import React, { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import './css/index.css';
+import App from './App.tsx';
+
+initReactFastclick();
+
+const rootElement = document.getElementById('root');
+if (!rootElement) throw new Error('Failed to find the root element');
+const root = createRoot(rootElement);
+root.render(
+    <StrictMode>
+        <App />
+    </ StrictMode>
+);
